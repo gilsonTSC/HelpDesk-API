87b9eab83eb04e5415868b253734517ed38f32b8
Gilson <gilson.costa@dce.ufpb.br>
2020-07-01 19:39:18 -0300

    Criação do projeto

---
+++ src/test/java/com/gilsontsc/helpDeskapi/HelpDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilsontsc.helpDeskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
